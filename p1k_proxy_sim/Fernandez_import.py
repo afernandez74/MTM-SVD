31ff19dda430c93f924ef04dbca61e4314b29341
Alejandro <afernandez@twodegreesadapt.com>
2025-03-22 13:44:56 -0500

    first commit p1k project

---
+++ p1k_proxy_sim/Fernandez_import.py
@@ -0,0 +1,153 @@
+#import records compiled by Fernandez et al., 2025
+
+import pickle
+import pandas as pd
+import scipy.io
+import os 
+import numpy as np
+import xarray as xr
+import lipd
+import matplotlib.pyplot as plt
+from import_funcs import lipd2df, annualize_data, annualize_data_AF, mask_data
+
+#%% pages2k summary table
+path = os.path.expanduser('~/mtm_local/pages2k/')
+p2k_summary_table = pd.read_csv(path+'/pages2k_proxy_summary_table.csv')
+
+# Fernandez summary table
+path = os.path.expanduser('~/mtm_local/proxy_data/fernandez_comp/metadata/Fernandez_summary_table.csv')
+AF_summary_table = pd.read_csv(path)
+
+#drop records whose data has not been added to the 'Usables' folder
+AF_archives = AF_summary_table.drop(AF_summary_table[AF_summary_table.Usables!='Yes'].index)
+
+AF_archives['resolution_int'] = AF_archives['resolution'].apply(lambda x: int(x) if not x.startswith('<') else 0)
+#%% Read csv files of data and create xarray datasets
+path = os.path.expanduser('~/mtm_local/proxy_data/fernandez_comp/usables/')
+files = os.listdir(path)
+files = [entry for entry in files if not entry.startswith('.')]
+#%% create xarray datasets and save to the appropriate directories
+for ix, arch in AF_archives.iterrows():
+
+    rez = arch.resolution_int
+    name = arch.Site_ID
+    print(name)
+    file = [i for i in files if name in i][0]
+    
+    # attributes for dataarrays
+    proxy_type = arch['Archive']
+    proxy_units = arch['units']
+    variable_name = arch['variable']
+    site_name = arch['Site Name']
+    archive_type = arch['Archive']
+    data_set_name = name
+    pages2kID = name
+    lat = arch['Lat']
+    lon = arch['Lon']
+    elev = np.nan
+    direction = arch['Direction']
+
+    dat = pd.read_csv(path + file, dtype={"proxy_data": float}, na_values = 'NA')
+    
+    # interpolate to annual, flip reverse timeseries and/or calc annual means if 
+    # finer than annual resolution
+    dat_annual = annualize_data_AF(dat, rez)
+    
+    #drop data after 1850
+    dat_annual = dat_annual.drop(dat_annual[dat_annual.Age_CE>1850].index)
+    # drop data before one of the cutoff years
+    if dat_annual.Age_CE[0] < 850:
+        dat_annual = dat_annual.drop(dat_annual[dat_annual.Age_CE<850].index)
+        start_yr = 850
+        
+    elif dat_annual.Age_CE[0] <= 1200:
+        dat_annual = dat_annual.drop(dat_annual[dat_annual.Age_CE<1200].index)
+        start_yr = 1200
+        
+    elif dat_annual.Age_CE[0] < 1400:
+        dat_annual = dat_annual.drop(dat_annual[dat_annual.Age_CE<1400].index)
+        start_yr = 1400
+   
+    elif dat_annual.Age_CE[0] <= 1600:
+        dat_annual = dat_annual.drop(dat_annual[dat_annual.Age_CE<1600].index)
+        start_yr = 1600
+
+    # create xarray dataarrays
+    year = dat_annual['Age_CE']
+    values = dat_annual['proxy_data']
+
+    
+    year_coord = xr.DataArray(year, dims='time', coords={'time': year}, attrs={'units': 'year AD'})
+    values_array = xr.DataArray(values, dims='time', coords={'time': year_coord}, attrs = {'units':archive_type +'_'+variable_name})
+                              
+    attrs_dic={'proxy_type':proxy_type,'units': proxy_units, 'site_name':site_name,
+    'variable_name':variable_name,'archive_type':archive_type,
+    'data_set_name':data_set_name,'pages2kID':pages2kID,
+    'lat':lat, 'lon':lon,'elev':elev, 'direction':direction}
+
+    # create xarray dataset
+    ds_i = xr.Dataset({'proxy_data': values_array})
+    ds_i.attrs = attrs_dic
+
+    # save .nc files
+    
+    if start_yr == 850:
+        save_path = os.path.expanduser(f"~/mtm_local/proxy_data/compiled_datasets/datasets{start_yr}_1850/")
+        file_name = f'AF_{data_set_name}.nc'
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)
+        
+        ds_i = ds_i.sel(time=slice(1200,1850))
+        save_path = os.path.expanduser("~/mtm_local/proxy_data/compiled_datasets//datasets1200_1850/") 
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)
+        
+        ds_i = ds_i.sel(time=slice(1400,1850))
+        save_path = os.path.expanduser("~/mtm_local/proxy_data/compiled_datasets//datasets1400_1850/") 
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)
+
+        ds_i = ds_i.sel(time=slice(1600,1850))
+        save_path = os.path.expanduser("~/mtm_local/proxy_data/compiled_datasets//datasets1600_1850/") 
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)   
+        
+    elif start_yr == 1200:
+        save_path = os.path.expanduser(f"~/mtm_local/proxy_data/compiled_datasets//datasets{start_yr}_1850/")
+        file_name = f'AF_{data_set_name}.nc'
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)
+
+        ds_i = ds_i.sel(time=slice(1400,1850))
+        save_path = os.path.expanduser("~/mtm_local/proxy_data/compiled_datasets//datasets1400_1850/") 
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)
+
+        ds_i = ds_i.sel(time=slice(1600,1850))
+        save_path = os.path.expanduser("~/mtm_local/proxy_data/compiled_datasets//datasets1600_1850/") 
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)   
+
+    elif start_yr == 1400:
+    
+        save_path = os.path.expanduser(f"~/mtm_local/proxy_data/compiled_datasets//datasets{start_yr}_1850/")
+        file_name = f'AF_{data_set_name}.nc'
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)
+    
+        ds_i = ds_i.sel(time=slice(1600,1850))
+        save_path = os.path.expanduser("~/mtm_local/proxy_data/compiled_datasets//datasets1600_1850/") 
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)   
+        
+    else:
+        save_path = os.path.expanduser(f"~/mtm_local/proxy_data/compiled_datasets//datasets{start_yr}_1850/")
+        file_name = f'AF_{data_set_name}.nc'
+        full_path = save_path + file_name
+        ds_i.to_netcdf(full_path)
+        
+    
+    
+    
+    
+    
---
+++ p1k_proxy_sim/pages2k_dat_plots.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 07:41:24 2023
+
+@author: afer
+"""
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os
+import xarray as xr 
+from scipy.stats import zscore
+import pandas as pd
+import cartopy.crs as ccrs
+
+# Set global settings for publication-style plots
+plt.rcParams.update({
+    'font.size': 14,
+    # 'font.family': 'serif',
+    # 'font.serif': ['Times New Roman'],
+    'figure.figsize': (8, 4),  # Customize figure size for publication
+    'figure.dpi': 300,  # Higher DPI for publication quality
+    'lines.linewidth': 2,  # Line width
+    'lines.color': 'black',  # Default line color
+    'axes.grid': True,  # Show grid
+    'axes.titlesize': 16,  # Axis title font size
+    'axes.labelsize': 14,  # Axis label font size
+    'xtick.labelsize': 12,  # X-tick label font size
+    'ytick.labelsize': 12,  # Y-tick label font size
+    'xtick.major.size': 6,  # Major tick size for x-axis
+    'ytick.major.size': 6,  # Major tick size for y-axis
+    'legend.fontsize': 12,  # Legend font size
+    'legend.loc': 'best',  # Best location for the legend
+    'legend.borderpad': 1.0,  # Padding around the legend
+    'grid.linestyle': '--',  # Dashed grid lines
+    'grid.color': 'gray',  # Grid line color
+    'grid.alpha': 0.5,  # Transparency of grid lines
+    'xtick.direction': 'in',  # Tick direction
+    'ytick.direction': 'in',  # Tick direction
+    'axes.labelpad': 10,  # Padding between axis labels and plot
+})
+
+#%% load pages2k datasets
+start_year = 850
+path = os.path.expanduser(f"~/mtm_local/pages2k/datasets{start_year}_1850/")
+files = os.listdir(path)
+files = [entry for entry in files if not entry.startswith('.')]
+files = sorted(files)
+
+# concatenate them into single dataset
+# ds_all = xr.open_mfdataset(f'{path}/*.nc', combine='nested', concat_dim = 'record')
+
+#%% organize data into archive types for plotting
+
+records = []
+
+# create dataframe with all metadata of all records
+archive_types = []
+
+for file in files:
+    
+    ds_i = xr.open_dataset(path+file)
+    
+    # Extract relevant attributes
+    record = {
+        "proxy_type": ds_i.attrs.get("proxy_type", None),
+        "units": ds_i.attrs.get("units", None),
+        "site_name": ds_i.attrs.get("site_name", None),
+        "variable_name": ds_i.attrs.get("variable_name", None),
+        "archive_type": ds_i.attrs.get("archive_type", None),
+        "data_set_name": ds_i.attrs.get("data_set_name", None),
+        "pages2kID": ds_i.attrs.get("pages2kID", None),
+        "lat": ds_i.attrs.get("lat", None),
+        "lon": ds_i.attrs.get("lon", None),
+        "elev": ds_i.attrs.get("elev", None),
+        "direction": ds_i.attrs.get("direction", None),
+    }
+    records.append(record)
+
+archives = pd.DataFrame(records)
+archive_types = set(archives.archive_type)
+#%%
+# read data, normalize and assign to archive_dat dictionary 
+# archive_dat keys-> archive types and values -> lists with all datasetsn normalized
+
+archive_dat = {}
+all_archs = []
+for archive_type in archive_types:
+    archive_dat[archive_type]=[]    
+    for file in files:
+        ds_i = xr.open_dataset(path+file)
+        
+        if ds_i.archive_type == archive_type:
+            attrs = ds_i.attrs
+            if attrs['direction'] == 'positive':
+                ds_i = (ds_i - ds_i.mean()) / ds_i.std() #standardize
+            else: 
+                ds_i = -1* (ds_i - ds_i.mean()) / ds_i.std() # standardize and flip
+            ds_i.attrs = attrs
+            archive_dat[archive_type].append(ds_i) #append to appropriate archive entry
+            all_archs.append(ds_i)
+#%%combine lists in dat_dic for easier handling of data and plotting
+
+dat_dic = {}
+for archive_type, list_i in archive_dat.items():
+    print(archive_type)
+    ds_list= []
+    for arch in list_i:
+        if archive_type == 'marine sediment':
+            print(arch)
+        var_name = arch.archive_type + '_' + arch.pages2kID
+        var_name = var_name.replace(' ','_') if '' in var_name else var_name
+        # change name of variable so the data is concatenateable
+        arch_rename = arch.rename({"proxy_data": var_name})
+        ds_list.append(arch_rename)
+    ds_new = xr.merge(ds_list)
+    dat_dic[archive_type] = ds_new
+        
+#%%
+
+for archive_type,ds in dat_dic.items():
+    mean_ts = ds.to_array(dim='arch').mean(dim='arch')
+    std_ts = ds.to_array(dim='arch').std(dim='arch')
+    
+    fig = plt.figure()
+    ax = fig.add_axes([0.1,0.1,0.5,0.8])
+    ax.tick_params(axis='x',which='major',direction='out',
+               pad=15, labelrotation=45)
+    ax.grid(True,which='major',axis='both')
+
+
+    p1 = mean_ts.plot(ax=ax,
+            linestyle = '-',
+            linewidth=2,
+            zorder = 10,
+            color = 'black',
+            label = f'mean {archive_type}')
+
+    # plot +1sd and -1sd shaded areas
+    p3 = ax.fill_between(std_ts.time.values, mean_ts+std_ts, mean_ts-std_ts,
+                    alpha=.5, linewidth=0.1, zorder = 2, color = 'lightgray',
+                    label = '\u00B1 \u03C3')
+    plt.title(f'{archive_type}_N={len(ds.data_vars)}')
+    plt.xlabel('Year AD')
+    plt.ylabel('Z-score proxy')
+
+#%%
+# Predefine the styles for each archive_type (color and marker and zorder)
+style_dict = {
+    'tree': ('green','^',1),
+    'borehole': ('orange', 'v', 5),      
+    'coral': ('red','h', 5),      
+    'documents': ('blue','s',3),   
+    'glacier ice': ('slategrey','o',3),  
+    'hybrid': ('indigo','X',5),  
+    'lake sediment': ('brown','d',3),   
+    'marine sediment': ('turquoise','*',3),
+    'sclerosponge': ('olive','<',5),
+    'speleothem': ('magenta','p',3),
+}
+# how many of each type there is 
+counts = archives.groupby('archive_type').size().reset_index(name='count')
+
+# Create the figure and map
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1, projection=ccrs.Robinson(central_longitude=80), facecolor=None)
+ax.set_global()
+ax.coastlines(zorder=1)
+ax.gridlines(linewidth=0.5,zorder=0.9)
+# Plot data points using the predefined styles
+for _, row in archives.iterrows():
+    archive_type = row['archive_type']
+    color,marker,z = style_dict[archive_type]
+    count = counts[counts.archive_type==archive_type]['count'].values[0]
+    ax.scatter(float(row['lon']), float(row['lat']),
+               color=color, 
+               marker=marker, 
+               transform=ccrs.PlateCarree(),  # Transform lat/lon to map projection
+               s=10,  # Marker size
+               label=f'{archive_type}, N={count}',
+               zorder=z
+                   )
+# Add a legend to the plot
+handles, labels = ax.get_legend_handles_labels()
+by_label = dict(zip(labels, handles))  # Remove duplicates if needed
+ax.legend(by_label.values(), by_label.keys(), 
+          loc='lower right', bbox_to_anchor=(1.05, 1), borderaxespad=0., fontsize=12)
+plt.title(f'Compiled archives for {start_year}-1850CE \nN={counts.sum(axis=0)[1]}')
+plt.show()
+#%% indiv plots 
+for arch in all_archs:
+    fig=plt.subplots()
+    arch.proxy_data.plot()
+    plt.title(arch.data_set_name)
+    plt.savefig(
+        os.path.expanduser(f'/Users/afer/mtm_local/pages2k/indiv_ts_plots{start_year}/')+
+        arch.data_set_name+'.png',
+        dpi=100)
+    plt.close()
